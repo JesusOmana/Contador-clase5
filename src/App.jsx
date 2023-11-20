0e60b83e6ae271fd653ef1c5e3f15b5a121052fa
jesus omana <jesus.omanamarquez@gmail.com>
2023-11-20 20:07:00 -0300

    "se creo un contador, clase 5"

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+  Box,
+  ChakraProvider,
+  Button,
+  Center,
+  Divider,
+  Spacer,
+} from "@chakra-ui/react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ChakraProvider>
+        <Box bg="#98e5ef">
+          <Center
+            fontSize={{ base: "3xl", sm: "6xl", md: "6xl", xl: "6xl" }}
+            color="black"
+            as="span"
+            flex="1"
+            textAlign="left"
+          >
+            Contador = {count}
+          </Center>
+        </Box>
+        <Box align="center" bg="#DDF2FD" w="100%" p={4} color="white">
+          <Button
+            mx="20px"
+            bg="black"
+            color="white"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            +
+          </Button>
+          <Button
+            bg="black"
+            color="white"
+            onClick={() => setCount((count) => count - 1)}
+          >
+            -
+          </Button>
+          <Button
+            ml="20px"
+            bg="black"
+            color="white"
+            onClick={() => setCount((count) => (count = 0))}
+          >
+            Restablecer
+          </Button>
+        </Box>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
